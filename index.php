c867d62bbf80a4a0497a98c482df68dd25b6b86a
moseskobmailcom <74140323+moseskobmailcom@users.noreply.github.com>
2020-11-08 18:28:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html> 
+<html> 
+
+<head> 
+	<title>Atlanta Florist</title> 
+	<style type="text/css"> 
+		html { 
+			overflow: auto; 
+		} 
+		
+		html, 
+		body, 
+		div, 
+		iframe { 
+			margin: 0px; 
+			padding: 0px; 
+			height: 100%; 
+			border: none; 
+		} 
+		
+		iframe { 
+			display: block; 
+			width: 100%; 
+			border: none; 
+			overflow-y: auto; 
+			overflow-x: hidden; 
+		} 
+	</style> 
+</head> 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ7V4MYRQ1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YQ7V4MYRQ1');
+</script>
+<body> 
+	<iframe src="https://www.hallsflowershop.com/"
+			frameborder="0"
+			marginheight="0"
+			marginwidth="0"
+			width="100%"
+			height="100%"
+			scrolling="auto"> 
+</iframe> 
+
+</body> 
+
+</html> 
